437bedaa169b05d74bce81e62409955a08379474
刘望晨 <liuwangchen-hj@huajiao.tv>
2019-04-25 17:59:15 +0800

    update proto

--- make.sh
+++ make.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-protoc \
-	-I=pkg/rpc/proto \
-	-I=$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-	--go_out=plugins=grpc:$GOPATH/src \
-	--grpc-gateway_out=logtostderr=true:$GOPATH/src \
-	--govalidators_out=$GOPATH/src \
-	health.proto
+for protofile in $(find pkg/rpc/proto -name "*.proto"); do
+	protoc \
+		-I=pkg/rpc/proto \
+		-I=$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+		--go_out=plugins=grpc:$GOPATH/src \
+		--grpc-gateway_out=logtostderr=true:$GOPATH/src \
+		--govalidators_out=$GOPATH/src \
+		$protofile
+done
 
 go build -o ./bin/rpc ./cmd/rpc/rpc.go
 go build -o ./bin/web ./cmd/web/web.go
