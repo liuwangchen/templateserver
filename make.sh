b82bc0dff2017be37141647b80bae1852258221a
刘望晨 <liuwangchen-hj@huajiao.tv>
2019-04-26 18:38:59 +0800

    update make.sh

--- make.sh
+++ make.sh
@@ -12,3 +12,5 @@ done
 go build -o ./bin/rpc ./cmd/rpc/rpc.go
 go build -o ./bin/web ./cmd/web/web.go
 go build -o ./bin/worker ./cmd/worker/worker.go
+
+#docker run -itd -p 3306:3306 -v ~/work/db/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_DATABASE=testdb mysql
